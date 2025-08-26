de78dbe385e5778d6cf13823aed0140c3e11b517
22127433 <anhtus2711@gmail.com>
2025-08-26 08:43:48 +0700

    Fix small bug

--- app/features/messages/core/settings.py
+++ app/features/messages/core/settings.py
@@ -1,15 +1,18 @@
 from functools import lru_cache
-from decouple import config
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 @lru_cache
 def get_settings():
     return {
-        "PUSHER_APP_ID": config("PUSHER_APP_ID", default="2021288"),
-        "PUSHER_KEY": config("PUSHER_KEY", default="9e079f4261072a6d7482"),
-        "PUSHER_SECRET": config("PUSHER_SECRET", default="9b9447e78cac364ca220"),
-        "PUSHER_CLUSTER": config("PUSHER_CLUSTER", default="ap1"),
-        "NEXT_PUBLIC_PUSHER_KEY": config("NEXT_PUBLIC_PUSHER_KEY", default="9e079f4261072a6d7482"),
-        "NEXT_PUBLIC_PUSHER_CLUSTER": config("NEXT_PUBLIC_PUSHER_CLUSTER", default="ap1")
+        "PUSHER_APP_ID": os.getenv("PUSHER_APP_ID"),
+        "PUSHER_KEY": os.getenv("PUSHER_KEY"),
+        "PUSHER_SECRET": os.getenv("PUSHER_SECRET"),
+        "PUSHER_CLUSTER": os.getenv("PUSHER_CLUSTER"),
+        "NEXT_PUBLIC_PUSHER_KEY": os.getenv("NEXT_PUBLIC_PUSHER_KEY"),
+        "NEXT_PUBLIC_PUSHER_CLUSTER": os.getenv("NEXT_PUBLIC_PUSHER_CLUSTER")
     }
 
 settings = get_settings()
